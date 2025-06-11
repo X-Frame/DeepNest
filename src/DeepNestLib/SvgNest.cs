19e1ac115ccba70223e3afb13991554f762cea12
Harrison Le Fevre <Harrison@xframe.com.au>
2025-06-11 13:04:20 +1200

    chore: remove old config class.

--- src/DeepNestLib/SvgNest.cs
+++ src/DeepNestLib/SvgNest.cs
@@ -1010,23 +1010,6 @@ namespace DeepNestLib
     {
         box, gravity, squeeze
     }
-    //public class SvgNestConfig
-    //{
-    //    public PlacementTypeEnum placementType = PlacementTypeEnum.box;
-    //    public double curveTolerance = 0.72;
-    //    public double scale = 25;
-    //    public double clipperScale = 10000000;
-    //    public bool exploreConcave = false;
-    //    public int mutationRate = 10;
-    //    public int populationSize = 10;
-    //    public int rotations = 4;
-    //    public double spacing = 100;
-    //    public double sheetSpacing = 500;
-    //    public bool useHoles = false;
-    //    public double timeRatio = 0.5;
-    //    public bool mergeLines = false;
-    //    public bool simplify;
-    //}
 
     public class DbCacheKey
     {
